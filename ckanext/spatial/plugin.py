8efb2afe470e35551c61ba9efed7a28ac7265ae7
Gregory von Nessi <gvn@Derp-E.local>
2016-10-31 15:24:45 +1100

    - Shift BBOX error for SOLR search to a debugging message to cut down on Sentry reports

--- ckanext/spatial/plugin.py
+++ ckanext/spatial/plugin.py
@@ -190,7 +190,7 @@ class SpatialQuery(p.SingletonPlugin):
                 if not (geometry['type'] == 'Polygon'
                    and len(geometry['coordinates']) == 1
                    and len(geometry['coordinates'][0]) == 5):
-                    log.error('Solr backend only supports bboxes, ignoring geometry {0}'.format(pkg_dict['extras_spatial']))
+                    log.debug('Solr backend only supports bboxes, ignoring geometry {0}'.format(pkg_dict['extras_spatial']))
                     return pkg_dict
 
                 coords = geometry['coordinates']
