ed2289b8a5a3dc182d9c17955eb74d14b6941f29
Gregory von Nessi <gvn@Derp-E.local>
2016-11-03 09:41:13 +1100

    - [SXTPDFINXZCB-207] Downgrading warning log entries to cut down on Sentry alerts

--- ckanext/spatial/model/harvested_metadata.py
+++ ckanext/spatial/model/harvested_metadata.py
@@ -116,12 +116,12 @@ class MappedXmlElement(MappedXmlObject):
         if self.multiplicity == "0":
             # 0 = None
             if values:
-                log.warn("Values found for element '%s' when multiplicity should be 0: %s",  self.name, values)
+                log.debug("Values found for element '%s' when multiplicity should be 0: %s",  self.name, values)
             return ""
         elif self.multiplicity == "1":
             # 1 = Mandatory, maximum 1 = Exactly one
             if not values:
-                log.warn("Value not found for element '%s'" % self.name)
+                log.debug("Value not found for element '%s'" % self.name)
                 return ''
             return values[0]
         elif self.multiplicity == "*":
